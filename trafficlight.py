0a813033f361ccd46df3f46e504cfc8309ac8af4
johnylogic <johns.baby@mca.christuniversity.in>
2022-07-12 23:43:58 +0530

    final commit for trafficlight.py

--- trafficlight.py
+++ trafficlight.py
@@ -1,50 +1,24 @@
 import serial
-
-#arduino = serial.Serial(port="COM23", baudrate="115200", timeout=.1)
 import torch
-import numpy as np
 import cv2
-from time import time
+import time
 
+class AmbulanceDetection:
 
-class MugDetection:
-
-    def __init__(self, capture_index, model_name):
+    def __init__(self, capture_index, capture_index2, capture_index3, model_name):
 
         self.capture_index = capture_index
-        self.model = self.load_model(model_name)
+        self.capture_index2 = capture_index2
+        self.capture_index3 = capture_index3
+
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_name, force_reload=True)
         self.classes = self.model.names
         print("classes = ", self.classes)
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("Using Device: ", self.device)
-
-    def get_video_capture(self):
-        """
-        Creates a new video streaming object to extract video frame by frame to make prediction on.
-        :return: opencv2 video capture object, with lowest quality frame available for video.
-        """
-
-        return cv2.VideoCapture(self.capture_index)
-
-    def load_model(self, model_name):
-        """
-        Loads Yolo5 model from pytorch hub.
-        :return: Trained Pytorch model.
-        """
-        if model_name:
-            model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_name, force_reload=True)
-            print("hello")
-        else:
-            model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-            print("hi")
-        return model
+        self.device = 'cpu'
+        self.arduino = serial.Serial(port="COM23", baudrate="115200", timeout=.1)
+        # self.arduino.write(bytes("hello arduino inside innit", 'utf-8'))
 
     def score_frame(self, frame):
-        """
-        Takes a single frame as input, and scores the frame using yolo5 model.
-        :param frame: input frame in numpy/list/tuple format.
-        :return: Labels and Coordinates of objects detected by model in the frame.
-        """
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
@@ -52,68 +26,100 @@ class MugDetection:
         return labels, cord
 
     def class_to_label(self, x):
-        """
-        For a given label value, return corresponding string label.
-        :param x: numeric label
-        :return: corresponding string label
-        """
+        print("classes[int(x)] = ", self.classes[int(x)])
         return self.classes[int(x)]
 
-    def plot_boxes(self, results, frame):
-        """
-        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-        :param results: contains labels and coordinates predicted by model on the given frame.
-        :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
-        """
+    def plot_boxes(self, results, frame, roadname):
         labels, cord = results
         n = len(labels)
         x_shape, y_shape = frame.shape[1], frame.shape[0]
         for i in range(n):
             row = cord[i]
-            if row[4] >= 0.3:
+            if row[4] >= 0.6:
                 x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(
                     row[3] * y_shape)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+                classfound = self.classes[int(labels[i])] # output from class_to_label function
+                if (classfound == "Ambulance2"):
+                    print("Ambulance detected in ", roadname)
+                    self.arduino.write(bytes(roadname, 'utf-8'))
 
+                cv2.putText(frame, classfound, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
         return frame
 
+    def initial_time_capture(self):   #to set the initial
+        global time_t
+        time_t = time.time()
+
+    def time_calculate(self):
+        global road
+
+        time_t2 = time.time()
+        finaltime = time_t2 - time_t
+
+        if finaltime > 5:  # set the green light time here
+            road = road + 1
+            print("road = ", road)
+            if road > 3:    # if road exceeds value 3 then reset to 1
+                road = 1
+
+            if road == 1:
+                print("Green Light for Road 1")
+                self.arduino.write(bytes("road1", 'utf-8'))
+            elif road == 2:
+                print("Green Light for Road 2")
+                self.arduino.write(bytes("road2", 'utf-8'))
+            elif road == 3:
+                print("Green Light for Road 3")
+                self.arduino.write(bytes("road3", 'utf-8'))
+            else:
+                print("Road mismatch")
+
+            self.initial_time_capture()     # after every interval the initial time should be reset
+
+
+
     def __call__(self):
-        """
-        This function is called when class is executed, it runs the loop to read the video frame by frame,
-        and write the output into a new file.
-        :return: void
-        """
-        cap = self.get_video_capture()
+        cap = cv2.VideoCapture(self.capture_index)
         assert cap.isOpened()
+        cap2 = cv2.VideoCapture(self.capture_index2)
+        assert cap2.isOpened()
+        cap3 = cv2.VideoCapture(self.capture_index3)
+        assert cap3.isOpened()
+
+        global road
+        road = 1
+
+        self.initial_time_capture()
 
         while True:
-
             ret, frame = cap.read()
-            assert ret
-
-            frame = cv2.resize(frame, (416, 416))
-
-            start_time = time()
+            frame = cv2.resize(frame, (500, 500))  # resize the output window
             results = self.score_frame(frame)
-            frame = self.plot_boxes(results, frame)
-
-            end_time = time()
-            fps = 1 / np.round(end_time - start_time, 2)
-            # print(f"Frames Per Second : {fps}")
-
-            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
-
+            frame = self.plot_boxes(results, frame, "road1")  # here 1 is the road number
             cv2.imshow('ROAD 1 Detection', frame)
+
+            ret, frame2 = cap2.read()
+            frame2 = cv2.resize(frame2, (500, 500))  # resize the output window
+            results2 = self.score_frame(frame2)
+            frame2 = self.plot_boxes(results2, frame2, "road2")   # here 2 is the road number
+            cv2.imshow('ROAD 2 Detection', frame2)
+
+            ret, frame3 = cap3.read()
+            frame3 = cv2.resize(frame3, (500, 500))  # resize the output window
+            results3 = self.score_frame(frame3)
+            frame3 = self.plot_boxes(results3, frame3, "road3")   # here 3 is the road number
+            cv2.imshow('ROAD 3 Detection', frame3)
+
+            self.time_calculate()
 
             if cv2.waitKey(5) & 0xFF == 27:
                 break
-
         cap.release()
 
 
 # Create a new object and execute.
-detector = MugDetection(capture_index='video1.mp4', model_name='best.pt')
+# detector = AmbulanceDetection(capture_index='video2.mp4', capture_index2="video3.mp4", capture_index3="video4.mp4", model_name='best.pt')
+detector = AmbulanceDetection(capture_index=0, capture_index2=1, capture_index3=2, model_name='best.pt')
 detector()
